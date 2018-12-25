e309717581e7a30ca6513ca52fd98d92c155331c
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-12-25 15:17:32 +0330

    Add test.py

---
+++ hic-pack/results/test.py
@@ -0,0 +1,7 @@
+"""
+    Created by Mohsen Naghipourfar on 2018-12-25.
+    Email : mn7697np@gmail.com or naghipourfar@ce.sharif.edu
+    Website: http://ce.sharif.edu/~naghipourfar
+    Github: https://github.com/naghipourfar
+    Skype: mn7697np
+"""
